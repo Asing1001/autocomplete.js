3870bc2a2bd648a97f4f92c235b1bb4b2b550dfb
Andy Chen <shinningstar1001@gmail.com>
2018-02-22 23:09:55 +0800

    Setup event/state for input

--- src/autoComplete.js
+++ src/autoComplete.js
@@ -1,4 +1,39 @@
 export default class autoComplete {
     constructor({ selector, items, getItemValue }) {
+        this.state = {
+            selectedIndex: -1,
+            show: false,
+            value: '',
+        }
+        this.props = { selector, items, getItemValue };
+        this.input = document.querySelector(selector);
+        this.input.addEventListener('focus', this.handleFocus.bind(this));
+        this.input.addEventListener('blur', this.handleBlur.bind(this));
+        this.input.addEventListener('keyup', this.handleKeyup.bind(this));
+        this.input.addEventListener('keydown', this.handleKeydown.bind(this));
+        this.suggestionUL = document.createElement('ul');
+        this.suggestionUL.classList.add('suggestion');
+        this.input.parentNode.insertBefore(this.suggestionUL, this.input.nextSibling);
+
+    }
+
+    handleFocus(e) {
+    }
+
+    handleBlur(e) {
+    }
+
+    handleKeyup(e) {
+    }
+
+    handleKeydown(e) {
+    }
+
+    setState(newState) {
+        this.state = { ...this.state, ...newState };
+        this.render();
+    }
+
+    render() {
     }
 }
